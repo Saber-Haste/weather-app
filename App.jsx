79efceac74242d6cce07cd9e73824f6e82e00ba7
Saber-Haste <binoychullikkal@protonmail.com>
2023-09-04 21:15:24 +0530

    init

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { View,Text,StyleSheet } from "react-native"
+
+const myComp = () => {
+  return (
+    <View style={{backgroundColor:}}>
+      <Text>
+        Hai
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+      flex: 1
+  },
+
+})
+export default myComp
